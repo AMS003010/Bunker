0d7366d4b432adedba7fe2b4add10152c0df914b
AMS <111883236+AMS003010@users.noreply.github.com>
2024-06-30 17:13:21 +0530

    added server side routes/controllers

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -14,17 +14,33 @@ const createFile = async (req, res) => {
     const user = await User.findOne({name: req.body.user});
     console.log("lin 4");
     console.log(user);
-    user.files.push(file.url);
+    user.files.push(file._id);
     await user.save();
     console.log("lin 5");
     res.status(201).send({data: file, message: "File created successfully"});
 };
 
-const getFiles = async (req,res) => {
+const getFiles = async (req, res) => {
     console.log("Incoming request data:", req.body.user);
-    const files = await User.find({name: req.body.user});
-    res.status(200).send({data: files});
-}
+    try {
+        const user = await User.findOne({ name: req.body.user });
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        }
+
+        const files = await Promise.all(
+            user.files.map(async (fileId) => {
+                const file = await Files.findById(fileId);
+                return file;
+            })
+        );
+
+        res.status(200).send({ data: files });
+    } catch (error) {
+        console.error("Error fetching files:", error);
+        res.status(500).send({ message: "Error fetching files" });
+    }
+};
 
 module.exports = {
     createFile,
